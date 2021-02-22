0e33ccf3128d5b394915806cc5d4c5fc5e4f1bc2
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-02-22 16:41:46 -0500

    Add Azure tenant and sub ID env variables

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  azure_tenant_id       = "ad28bd3a-04b1-4d27-ae0e-97bc62310517"
+  azure_subscription_id = "83dd4203-c4b5-4945-a690-4940ece46a7d"
+}
--- stacks.tf
+++ stacks.tf
@@ -9,3 +9,17 @@ resource "spacelift_stack" "azure_subscription" {
   autodeploy = true
   labels     = ["managed", "depends-on:${data.spacelift_current_stack.this.id}"]
 }
+
+resource "spacelift_environment_variable" "azure_tenant_id" {
+  stack_id   = spacelift_stack.azure_subscription.id
+  name       = "ARM_TENANT_ID"
+  value      = local.azure_tenant_id
+  write_only = false
+}
+
+resource "spacelift_environment_variable" "azure_subscription_id" {
+  stack_id   = spacelift_stack.azure_subscription.id
+  name       = "ARM_SUBSCRIPTION_ID"
+  value      = local.azure_subscription_id
+  write_only = false
+}
